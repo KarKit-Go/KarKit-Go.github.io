48298427bdc9b78e5f0e9fce16ef83d3f68b36cc
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-01-04 04:24:19 +0800

    feat: the component of Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,33 @@
 import "../styles/Header.scss";
+import { Link } from "react-router-dom";
+
+interface INavItem {
+  target: string;
+  text: string;
+}
+
+const NavItem = (props: INavItem) => {
+  const { target, text } = props;
+  return (
+    <li className="nav-item">
+      <Link to={target}>{text}</Link>
+    </li>
+  );
+};
+
 const Header = () => {
   return (
     <div className="wrapper">
-      <div className="nav-logo">KarKit</div>
-      <div className="nav-buttons">
-        <div className="nav-button">首页</div>
-        <div className="nav-button">设计流程</div>
-        <div className="nav-button">工具介绍</div>
-        <div className="nav-button">操作手册</div>
-        <div className="nav-button">设计团队</div>
-      </div>
+      <div className="logo">KarKit</div>
+      <nav>
+        <ol>
+          <NavItem target="/" text="首页" />
+          <NavItem target="/" text="设计流程" />
+          <NavItem target="/" text="工具介绍" />
+          <NavItem target="/" text="操作手册" />
+          <NavItem target="/" text="设计团队" />
+        </ol>
+      </nav>
     </div>
   );
 };
