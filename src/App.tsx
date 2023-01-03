1e837fa1ed96072b4d88e880833abc243e25f53d
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-01-04 04:24:52 +0800

    feat: update the structure of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { HashRouter as Router, Link } from "react-router-dom";
 
 // import components made by myself
 import Header from "./components/Header";
+import Homepage from "./components/Homepage";
 
 // import style
 import "./styles/App.scss";
 
 function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Header></Header>
+      <Homepage></Homepage>
+    </Router>
+  );
 }
 
 export default App;
