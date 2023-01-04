092f7f9ccf9d8c8caa27c5fa9420e6f74eec3868
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-01-04 13:25:17 +0800

    feat: update loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { HashRouter as Router, Link } from "react-router-dom";
+//
+import Loader from "react-loaders";
 
 // import components made by myself
 import Header from "./components/Header";
-import Homepage from "./components/Homepage";
 
 // import style
 import "./styles/App.scss";
 
+const Homepage = lazy(() => import("./components/Homepage"));
+
+const Spinner = () => {
+  return (
+    <div className="loc">
+      <Loader type="pacman" active />
+    </div>
+  );
+};
+
 function App() {
   return (
     <Router>
-      <Header></Header>
-      <Homepage></Homepage>
+      <Suspense fallback={<Spinner />}>
+        <Header />
+        <Homepage />
+      </Suspense>
     </Router>
   );
 }
