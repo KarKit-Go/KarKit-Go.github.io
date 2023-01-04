60ff40875869adfd33686d9c0d33bfe8ed33c4c5
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-01-04 18:17:26 +0800

    feat: finished the navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,71 @@
+import { useState } from "react";
 import "../styles/Header.scss";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 
 interface INavItem {
   target: string;
   text: string;
+  activeID: string;
+  changeActive: (text: string) => void;
 }
 
 const NavItem = (props: INavItem) => {
-  const { target, text } = props;
+  const { target, text, activeID, changeActive } = props;
+  const className = classNames("nav-item", { active: activeID === text });
   return (
-    <li className="nav-item">
+    <li className={className} onClick={() => changeActive(text)}>
       <Link to={target}>{text}</Link>
     </li>
   );
 };
 
 const Header = () => {
+  const [activeID, setActiveID] = useState<string>("");
+
+  const changeActive = (text: string) => {
+    setActiveID(text);
+  };
+
   return (
     <div className="wrapper">
-      <div className="logo">KarKit</div>
+      <div className="logo">
+        <Link to="/" onClick={() => changeActive("首页")}>
+          KarKit
+        </Link>
+      </div>
       <nav>
         <ol>
-          <NavItem target="/" text="首页" />
-          <NavItem target="/" text="设计流程" />
-          <NavItem target="/" text="工具介绍" />
-          <NavItem target="/" text="操作手册" />
-          <NavItem target="/" text="设计团队" />
+          <NavItem
+            target="/"
+            text="首页"
+            activeID={activeID}
+            changeActive={changeActive}
+          />
+          <NavItem
+            target="/design"
+            text="设计流程"
+            activeID={activeID}
+            changeActive={changeActive}
+          />
+          <NavItem
+            target="/tool"
+            text="工具介绍"
+            activeID={activeID}
+            changeActive={changeActive}
+          />
+          <NavItem
+            target="/book"
+            text="操作手册"
+            activeID={activeID}
+            changeActive={changeActive}
+          />
+          <NavItem
+            target="/team"
+            text="设计团队"
+            activeID={activeID}
+            changeActive={changeActive}
+          />
         </ol>
       </nav>
     </div>
