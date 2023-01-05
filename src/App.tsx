28065dc6e81b7c777ba1cda71cfeafd1064f7742
wujinhjun <wujinhjun@sjtu.edu.cn>
2023-01-05 21:15:17 +0800

    feat: Route system

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React, { Suspense, lazy } from "react";
-import { HashRouter as Router, Link } from "react-router-dom";
+import { HashRouter as Router, Link, Route, Routes } from "react-router-dom";
 //
 import Loader from "react-loaders";
 
 // import components made by myself
 import Header from "./components/Header";
+import Design from "./components/Design";
+import Tool from "./components/Tool";
+import Book from "./components/Book";
+import Team from "./components/Team";
 
 // import style
 import "./styles/App.scss";
 
 const Homepage = lazy(() => import("./components/Homepage"));
 
-const Spinner = () => {
+export const Spinner = () => {
   return (
     <div className="loc">
       <Loader type="pacman" active />
@@ -24,7 +28,13 @@ function App() {
     <Router>
       <Suspense fallback={<Spinner />}>
         <Header />
-        <Homepage />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/design" element={<Design />} />
+          <Route path="/tool" element={<Tool />} />
+          <Route path="/book" element={<Book />} />
+          <Route path="/Team" element={<Team />} />
+        </Routes>
       </Suspense>
     </Router>
   );
